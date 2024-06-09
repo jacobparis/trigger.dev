82ad3bd0880110f990c1dbaef6b91be232cf5930
Jacob Paris <jacob@jacobparis.com>
2024-06-09 16:03:30 -0400

    fix: add knip to core

--- packages/core/src/v3/index.ts
+++ packages/core/src/v3/index.ts
@@ -1,19 +1,3 @@
-export * from "./apiClient";
-export * from "./apiClient/types";
-export * from "./apiClient/pagination";
-export type { ApiPromise } from "./apiClient/core";
-export * from "./apiClient/errors";
-export * from "./clock-api";
-export * from "./errors";
-export * from "./limits";
-export * from "./logger-api";
-export * from "./runtime-api";
-export * from "./task-context-api";
-export * from "./apiClientManager-api";
-export * from "./schemas";
-export { SemanticInternalAttributes } from "./semanticInternalAttributes";
-export * from "./task-catalog-api";
-export * from "./types";
 export {
   formatDuration,
   formatDurationInDays,
@@ -23,35 +7,4 @@ export {
   nanosecondsToMilliseconds,
 } from "./utils/durations";
 
-export { TriggerTracer } from "./tracer";
-
-export type { LogLevel } from "./logger/taskLogger";
-
-export { eventFilterMatches } from "../eventFilterMatches";
-export {
-  flattenAttributes,
-  primitiveValueOrflattenedAttributes,
-  unflattenAttributes,
-  NULL_SENTINEL,
-} from "./utils/flattenAttributes";
-export { omit } from "./utils/omit";
-export {
-  calculateNextRetryDelay,
-  calculateResetAt,
-  defaultFetchRetryOptions,
-  defaultRetryOptions,
-} from "./utils/retries";
-export { accessoryAttributes } from "./utils/styleAttributes";
-
-export { detectDependencyVersion } from "./utils/detectDependencyVersion";
-export {
-  conditionallyExportPacket,
-  conditionallyImportPacket,
-  createPacketAttributes,
-  createPacketAttributesAsJson,
-  packetRequiresOffloading,
-  parsePacket,
-  prettyPrintPacket,
-  stringifyIO,
-  type IOPacket,
-} from "./utils/ioSerialization";
+export { conditionallyImportPacket } from "./utils/ioSerialization";
