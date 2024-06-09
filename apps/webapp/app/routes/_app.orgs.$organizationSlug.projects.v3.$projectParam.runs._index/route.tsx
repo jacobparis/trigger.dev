cfa4cecd3587848d8419cf81ef18491cff521ac0
Jacob Paris <jacob@jacobparis.com>
2024-06-09 14:55:55 -0400

    fix: remove unused files

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.v3.$projectParam.runs._index/route.tsx
+++
@@ -1,9 +0,0 @@
-import { formatDuration } from "@trigger.dev/core/v3";
-
-export default function Page() {
-  <span>
-    {formatDuration(new Date("2024-05-01"), new Date(), {
-      style: "short",
-    })}
-  </span>;
-}
