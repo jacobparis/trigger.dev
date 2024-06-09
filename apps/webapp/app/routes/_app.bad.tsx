6cc84a90a429c146ea15a1bd39fd7dc94b76c0d2
Jacob Paris <jacob@jacobparis.com>
2024-06-09 18:30:56 -0400

    fix: delete core

--- apps/webapp/app/routes/_app.bad.tsx
+++ apps/webapp/app/routes/_app.bad.tsx
@@ -1,11 +1,14 @@
-import { formatDuration } from "@trigger.dev/core/v3";
+// import { formatDuration } from "@trigger.dev/core/v3";
+import { Readable } from "node:stream";
+console.log(Readable);
 
 export default function AppGood() {
   return (
     <span>
-      {formatDuration(new Date("2024-05-01"), new Date(), {
+      <h1>Bad Route</h1>
+      {/* {formatDuration(new Date("2024-05-01"), new Date(), {
         style: "short",
-      })}
+      })} */}
     </span>
   );
 }
